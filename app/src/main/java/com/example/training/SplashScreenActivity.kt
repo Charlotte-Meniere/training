efb50f84b7dde201b816e3920e07ca581af3b6e2
Charlotte-Rhaegal <92514617+Charlotte-Rhaegal@users.noreply.github.com>
2021-12-20 14:20:24 +0100

    SPLASH

--- app/src/main/java/com/example/training/SplashScreenActivity.kt
+++ app/src/main/java/com/example/training/SplashScreenActivity.kt
@@ -2,22 +2,23 @@ package com.example.training
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashScreenActivity {
-
-        class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
             override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
 
                 setContentView(R.layout.activity_splash_screen)
 
-                logoview.alpha = 0f
-                logoview.animate().setDuration(1500).alpha(1f).withEndAction{
+//ici Logoview est l'id Imagewiew est le type et on le situe dans le dossier avec le findwiew
+                var logoview : ImageView = findViewById(R.id.logoview)
+
+                logoview.alpha = 1f
+                logoview.animate().setDuration(2000).alpha(0f).withEndAction{
                     val i = Intent(this, MainActivity::class.java)
                     startActivity(i)
-                    overridePendingTransition(android.R.anim.fade_out)
                     finish()
                 }
             }
-        }}
+        }
